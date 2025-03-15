9065458547e7aeef7b5f776936390d038767baeb
Raffaele <mori.raffaele@gmail.com>
2025-03-15 18:04:50 +0000

    Project completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,29 @@ export default function Home() {
       <h2>
         Server side render VS Client side render VS Static site generation
       </h2>
+      <p>Main concepts</p>
+      <ol>
+        <li>
+          <abbr title="CSR">
+            <b>C</b>lient <b>S</b>ide <b>R</b>endering
+          </abbr>
+        </li>
+        <li>
+          <abbr title="SSG">
+            <b>S</b>tatic <b>S</b>ite <b>G</b>eneration
+          </abbr>
+        </li>
+        <li>
+          <abbr title="SSR">
+            <b>S</b>erver <b>S</b>ite <b>R</b>endering
+          </abbr>
+        </li>
+        <li>
+          <abbr title="RSC">
+            <b>R</b>eact <b>S</b>erver <b>C</b>omponent
+          </abbr>
+        </li>
+      </ol>
     </div>
   );
 }
